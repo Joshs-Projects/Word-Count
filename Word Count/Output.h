e5a671c5fc36718060c4af02fb3cfe484bc6e140
Joshs-Projects <74363804+Joshs-Projects@users.noreply.github.com>
2021-04-06 12:50:34 +0100

    Added in average words per day.

--- Word Count/Output.h
+++ Word Count/Output.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "CSVFile.h"
+#include <tuple>
 
 class Output : public CSVFile {
 
@@ -23,6 +24,7 @@ public:
 		return 1;
 	}
 
+	//This gets the version that the csv file has most recently
 	int getVersion() {
 		return data.size() - 1;
 	}
@@ -34,6 +36,67 @@ public:
 		else {
 			return stoi(data[getVersion()][3]);
 		}
+	}
 
+	int getAverageWordsPerSession() {
+		int total = 0;
+		for (int i = 1; i < data.size(); i++) {
+			total = total + stoi(data[i][2]);
+		}
+		return (total / getVersion());
+	}
+
+	std::vector<std::tuple<std::string, int>> getAverageWordsPerDay() {
+		//First entry is the date second is the number of sessions in that day third is the total for the day
+		std::vector <std::tuple <std::string, int, int>> daySessionsTotal = {};
+		std::tuple <std::string, int, int> tempTuple;
+		/*std::get<0>(tempTuple) = data[1][0];
+		std::get<1>(tempTuple) = 1;
+		std::get<2>(tempTuple) = stoi(data[1][2]);
+		daySessionsTotal.push_back(tempTuple);*/
+
+		std::vector<std::tuple <std::string, int>> dateAverage = {};
+		std::tuple <std::string, int> tempTupleAverage;
+		
+		for (int i = 1; i < data.size(); i++) {
+			bool found = false;
+			for (int q = 0; q < daySessionsTotal.size(); q++) {
+				if (data[i][0].compare(std::get<0>(daySessionsTotal[q])) == 0) {
+					std::get<1>(daySessionsTotal[q]) = std::get<1>(daySessionsTotal[q]) + 1;
+					std::get<2>(daySessionsTotal[q]) = std::get<2>(daySessionsTotal[q]) + stoi(data[i][2]);
+					found = true;
+				}
+			}
+
+			if (found == false) {
+				std::get<0>(tempTuple) = data[i][0];
+				std::get<1>(tempTuple) = 1;
+				std::get<2>(tempTuple) = stoi(data[i][2]);
+				daySessionsTotal.push_back(tempTuple);
+			}
+		}
+
+		/*for (int q = 0; q < daySessionsTotal.size(); q++) {
+			for (int i = 1; i < data.size(); i++) {
+				if (data[i][0] == std::get<0>(daySessionsTotal[q])) {
+					std::get<1>(daySessionsTotal[q]) = std::get<1>(daySessionsTotal[q]) + 1;
+					std::get<2>(daySessionsTotal[q]) = std::get<2>(daySessionsTotal[q]) + stoi(data[i][2]);
+				}
+				else {
+					std::get<0>(tempTuple) = data[i][0];
+					std::get<1>(tempTuple) = 1;
+					std::get<2>(tempTuple) = stoi(data[i][2]);
+					daySessionsTotal.push_back(tempTuple);
+				}
+			}
+		}*/
+
+		for (int i = 0; i < daySessionsTotal.size(); i++) {
+			std::get<0>(tempTupleAverage) = std::get<0>(daySessionsTotal[i]);
+			std::get<1>(tempTupleAverage) = std::get<2>(daySessionsTotal[i])/(std::get<1>(daySessionsTotal[i]));
+			dateAverage.push_back(tempTupleAverage);
+		}
+
+		return dateAverage;
 	}
 };
--- Word Count/Source.cpp
+++ Word Count/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <tuple>
 
 //#include "CSVFile.h"
 #include "Output.h"
@@ -16,6 +17,7 @@ std::string getCurrentDate() {
 	std::cin >> month;
 	std::cout << "Please enter the day: ";
 	std::cin >> day;
+	std::cout << std::endl;
 
 	return (day + "-" + month + "-" + year);
 }
@@ -30,39 +32,54 @@ int getCurrentVersion() {
 int main() {
 	std::string Entry = "";
 	//Edit an entry could take excel grid reference for ease of use
-	std::cout << "1)Update" << std::endl << "2)Edit an entry" << std::endl << "QUIT to quit" << std::endl;
+	std::cout << "1)Update" << std::endl << "2)Get Statistics" << std::endl << "QUIT to quit" << std::endl;
 	std::cin >> Entry;
 
-	while (Entry != "1" && Entry != "2" && Entry != "QUIT") {
-		std::cout << "Please enter either 1, 2 or QUIT" << std::endl;
-		std::cout << "1)Update" << std::endl << "2)Edit an entry" << std::endl << "QUIT to quit" << std::endl;
-		std::cin >> Entry;
-	}
+	//while (Entry != "QUIT") {
+		while (Entry != "1" && Entry != "2" && Entry != "QUIT") {
+			std::cout << "Please enter either 1, 2 or QUIT" << std::endl;
+			std::cout << "1)Update" << std::endl << "2)Edit an entry" << std::endl << "QUIT to quit" << std::endl;
+			std::cin >> Entry;
+		}
+	
+		//Update the output.csv file
+		if (Entry == "1") {
+			//Ask the user for the most recent version number to be bale to loop through multiple versions if necessary
+			int currentVersion = getCurrentVersion();
 
-	if (Entry == "QUIT") {
-		return 1;
-	}
-	else if (Entry == "1") {
-		//Ask the user for the most recent version number to be bale to loop through multiple versions if necessary
-		int currentVersion = getCurrentVersion();
+			Output outputCSV = Output("output");
+			outputCSV.CSVRead();
+			Document inputText = Document("Draft ", outputCSV.getVersion());
+			//inputText.ReadTextFile();
+			for (int i = outputCSV.getVersion() + 1; i <= currentVersion; i++) {
+				inputText.SetTextFilename("Draft ", i);
+				inputText.ReadTextFile();
+				std::cout << "Please enter the date for Draft " + std::to_string(i) << std::endl;
+				outputCSV.setNewRow({ getCurrentDate(), "", std::to_string(inputText.getNumberOfWords() - outputCSV.getWordCount()), std::to_string(inputText.getNumberOfWords()) });
 
-		Output outputCSV = Output("output");
-		outputCSV.CSVRead();
-		Document inputText = Document("Draft ", outputCSV.getVersion());
-		//inputText.ReadTextFile();
-		for (int i = outputCSV.getVersion()+1; i <= currentVersion; i++) {
-			inputText.SetTextFilename("Draft ", i);
-			inputText.ReadTextFile();
-			std::cout << "Please enter the date for Draft " + std::to_string(i) << std::endl;
-			outputCSV.setNewRow({ getCurrentDate(), "", std::to_string(inputText.getNumberOfWords()-outputCSV.getWordCount()), std::to_string(inputText.getNumberOfWords()) });
+			}
+
+			//outputCSV.setEntry(outputCSV.getVersion(), 3, std::to_string(inputText.getNumberOfWords()));
+			outputCSV.CSVWrite();
+		}
+		//Display all the statistics
+		else if (Entry == "2") {
+
+			Output outputCSV = Output("output");
+			outputCSV.CSVRead();
+
+			std::vector<std::tuple<std::string, int>> dateAverage = outputCSV.getAverageWordsPerDay();
+
+			std::cout << "This is your average new words per session: " + std::to_string(outputCSV.getAverageWordsPerSession()) << std::endl;
+			std::cout << "This is your average new words per day: " << std::endl;
+			for (int i = 0; i < dateAverage.size(); i++) {
+				std::cout << "On " << std::get<0>(dateAverage[i]) << " you averaged " << std::get<1>(dateAverage[i]) << " words." << std::endl;
+			}
+			std::cout << "You've also typed " << outputCSV.getWordCount() << " words " << std::endl;
 			
 		}
 
-		
-		//outputCSV.setEntry(outputCSV.getVersion(), 3, std::to_string(inputText.getNumberOfWords()));
-		outputCSV.CSVWrite();
 
-	}
-
+	//}
 	return 1;
 }
